f4cbbc3ffc82cf1285d6f7bc605f5121fdb619ee
James Balamuta <balamut2@illinois.edu>
2020-07-21 22:32:50 -0500

    Add taylor-manifest-anxiety-scale-data

---
+++ man-roxygen/papers-taylor-manifest-anxiety-scale.R
@@ -0,0 +1,8 @@
+#' @references
+#' OpenPsychometrics. 2012. *Taylor Manifest Anxiety Scale*.
+#' <http://openpsychometrics.org/_rawdata/TMA.zip>.
+#'
+#' Taylor, J. A. (1953). *A personality scale of manifest anxiety*.
+#' The Journal of Abnormal and Social Psychology, 48(2), 285–290.
+#' <https://doi.org/10.1037/h0056264>
+
