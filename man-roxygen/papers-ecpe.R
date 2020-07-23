e19988739f6b7c3a5cf96d6b81711f95b2417b12
James J Balamuta <coatless@users.noreply.github.com>
2020-07-23 18:57:06 -0500

    Improve pkgdown website by grouping alike data and linking out to papers (#9)
    * README update
    
    * Add a convenience helper for tidying up the .bib file
    
    * Update bibliography entry
    
    * Add a few more papers that use data in this package.
    
    * Add APA 7th edition single-spaced bibliography csl style from
    https://www.zotero.org/styles?q=apa
    
    * Add DOI's and another paper
    
    * Clean up bibliography by deleting abstract, fixing title and adding another doi
    
    * Block the apa-single-spaced.csl from being shipped with the package.
    
    * Update the README with new style and additional citations
    
    * Roll out the new format inside of the documentation for references.
    
    * Update the pkgdown configuration with a breakdown of data.
    
    * Fix missing andrew entry

--- man-roxygen/papers-ecpe.R
+++ man-roxygen/papers-ecpe.R
@@ -1,4 +1,17 @@
 #' @references
-#' Templin, J. and Hoffman, L. (2013), Obtaining Diagnostic Classification Model
-#' Estimates Using Mplus. Educational Measurement: Issues and Practice, 32: 37-50.
-#' <doi:10.1111/emip.12010>
+#' **Data originated from:**
+#'
+#' - Templin, J., & Bradshaw, L. (2014). Hierarchical diagnostic
+#'   classification models: A family of models for estimating and testing
+#'   attribute hierarchies. *Psychometrika*, *79*(2), 317–339.
+#'   <https://doi.org/10.1007/s11336-013-9362-0>
+#' - Templin, J., & Hoffman, L. (2013). Obtaining diagnostic classification
+#'   model estimates using mplus. *Educational Measurement: Issues and
+#'   Practice*, *32*(2), 37–50. <https://doi.org/10.1111/emip.12010>
+#'
+#' **Data used in:**
+#'
+#' - Culpepper, S. A., & Chen, Y. (2019). Development and application of an
+#'   exploratory reduced reparameterized unified model. *Journal of
+#'   Educational and Behavioral Statistics*, *44*(1), 3–24.
+#'   <https://doi.org/10.3102/1076998618791306>
