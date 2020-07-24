6dcfe31c05368b8645e85909c5df72df167a513d
James Balamuta <balamut2@illinois.edu>
2020-07-23 22:52:05 -0500

    Add oracle Q matrices...
    Use old-school data generation techniques to combine data into a single documentation file.

---
+++ R/qmatrix-oracle-data.R
@@ -0,0 +1,24 @@
+#' @title Oracle Q Matrices
+#'
+#' @description
+#' Pre-generated identified Q matrices used in simulation studies to verify
+#' recovery.
+#'
+#' @format
+#' A `matrix` with varying numbers of traits (`K`) and items (`J`).
+#'
+#' Specifically:
+#'
+#' - `qmatrix_oracle_k3_j20`: 20 items and 3 traits.
+#' - `qmatrix_oracle_k4_j20`: 20 items and 4 traits.
+#' - `qmatrix_oracle_k5_j30`: 30 items and 5 traits.
+#'
+#' @details
+#' Each entry in the matrix is either `1`, if the item uses the skill, or `0`, if
+#' the item does not use the skill.
+#'
+#' @name qmatrix_oracle
+#' @aliases qmatrix_oracle_k3_j20 qmatrix_oracle_k4_j20 qmatrix_oracle_k5_j30
+#' @docType data
+#' @keywords datasets
+NULL
