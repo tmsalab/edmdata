543de362d0e487b18ed2e57de447cdac4458c63e
James Balamuta <balamut2@illinois.edu>
2020-07-25 16:21:55 -0500

    Add Matrix Reasoning data and documentation

---
+++ R/matrix-reasoning-data.R
@@ -0,0 +1,20 @@
+#' Experimental Matrix Reasoning Test Item Responses
+#'
+#' @template var-docs-matrix-reasoning
+#'
+#' @section Answer coding:
+#' The subjects answered a set of assessment items seeking to determine
+#' their matrix reasoning abilities. Subjects that answered with a value between
+#' 0 to 7 were marked as incorrect. Subjects who answered a question with 10
+#' selected the correct answer and, thus, were marked as correct.
+#'
+#' @section Notes:
+#'
+#' From the OpenPsychometrics' code book that accompanied the data, they noted:
+#'
+#' 1. The possible answers were presented in two rows of four with
+#'    a random order for each participant.
+#' 2. The collection of this data was of mediocre quality.
+#'
+#' @template papers-matrix-reasoning
+"items_matrix_reasoning"
