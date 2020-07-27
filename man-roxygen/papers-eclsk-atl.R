9db9da51167a5b18c3d6f1174b9d05f3d38a0dae
James Balamuta <balamut2@illinois.edu>
2020-07-27 18:33:25 -0500

    Add ECLS-K ATL data and documentation

---
+++ man-roxygen/papers-eclsk-atl.R
@@ -0,0 +1,14 @@
+#' @references
+#' **Data originated from:**
+#'
+#' - NCES. (2010). *Early childhood longitudinal study, kindergarten class of
+#'   1998-99 (ecls-k) kindergarten through fifth grade approaches to learning
+#'   and self-description questionnaire (sdq) items and public-use data
+#'   files*. <https://nces.ed.gov/pubsearch/pubsinfo.asp?pubid=2010070>
+#'
+#' **Data used in:**
+#'
+#' - Culpepper, S. (2019). An exploratory diagnostic model for ordinal
+#'   responses with binary attributes: Identifiability and estimation.
+#'   *Psychometrika*, *84*(4), 921–940.
+#'   <https://doi.org/10.1007/s11336-019-09683-4>
