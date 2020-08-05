be893be2be825054ee382463b14dfe15a83c8daf
James Balamuta <balamut2@illinois.edu>
2020-08-04 22:11:20 -0500

    Add documentation for spm-ls

---
+++ man-roxygen/papers-spm-ls.R
@@ -0,0 +1,21 @@
+#' @references
+#'
+#' **Assessment Design:**
+#'
+#' - Raven, J. C. (1941). Standardization of progressive matrices, 1938.
+#'  *British Journal of Medical Psychology*, *19*(1), 137–150.
+#'  <https://doi.org/10.1111/j.2044-8341.1941.tb00316.x>
+#'
+#' **Data originated from:**
+#'
+#' - Myszkowski, N., & Storme, M. (2018). A snapshot of g? Binary and
+#'   polytomous item-response theory investigations of the last series of the
+#'   standard progressive matrices (spm-ls). *Intelligence*, *68*, 109–116.
+#'   <https://doi.org/10.1016/j.intell.2018.03.010>
+#' - Robitzsch, A. (2020). Regularized latent class analysis for polytomous
+#'   item responses: An application to spm-ls data. *Preprint*.
+#'   <https://doi.org/10.20944/preprints202007.0269.v1>
+#'
+#' **Data used in:**
+#'
+#' - TBA
---
+++ man-roxygen/var-docs-spm-ls.R
@@ -0,0 +1,16 @@
+#' @details
+#' Items with the correct answer response based off of Table 9 of the
+#' Robitzsch (2020) pre-print paper.
+#'
+#' - SPM1: **7**
+#' - SPM2: **6**
+#' - SPM3: **8**
+#' - SPM4: **2**
+#' - SPM5: **1**
+#' - SPM6: **5**
+#' - SPM7: **1**
+#' - SPM8: **6**
+#' - SPM9: **3**
+#' - SPM10: **2**
+#' - SPM11: **4**
+#' - SPM12: **5**
