2b963744d38c6d06552c7754de664f6d9adcec09
James Balamuta <balamut2@illinois.edu>
2021-08-06 21:07:13 -0500

    Add help file

---
+++ man-roxygen/papers-pisa12-us-math.R
@@ -0,0 +1,21 @@
+#' @references
+#'
+#' **Assessment Design:**
+#'
+#' - PISA Test Overview: <https://www.oecd.org/pisa/aboutpisa/>
+#' - PISA 2012 Mathematics Items:
+#'  <https://www.oecd.org/pisa/test/PISA%202012%20items%20for%20release_ENGLISH.pdf>
+#'
+#' **Data originated from:**
+#'
+#' - Paul Bailey, Ahmad Emad, Huade Huo, Michael Lee, Yuqi Liao, Alex Lishinski, Trang Nguyen,
+#'   Qingshu Xie, Jiao Yu, Ting Zhang, Eric Buehler, Jeppe Bundsgaard,
+#'   Ren C'deBaca and Anders Astrup Christensen (2021).
+#'   EdSurvey: Analysis of NCES Education Survey
+#'   and Assessment Data. R package version 2.7.0.
+#'   <https://CRAN.R-project.org/package=EdSurvey>
+#'
+#' **Data used in:**
+#'
+#' - TBA
+
