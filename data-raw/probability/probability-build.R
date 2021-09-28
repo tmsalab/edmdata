faba9102c91cb32a157ce31d1a6e0adcf6b714cc
James J Balamuta <coatless@users.noreply.github.com>
2021-09-27 19:44:01 -0500

    Address NA values in pks::probability data set (#23)
    * Roll probability overviews
    
    * Fix linking

--- data-raw/probability/probability-build.R
+++ data-raw/probability/probability-build.R
@@ -12,10 +12,15 @@ data(probability, package = "pks")
 ## Format Item Data ----
 
 # Extract out the first set of 12 problems and their true/false coded answers.
-matched_part_one_columns = grepl("b1[0-9]+", colnames(probability))
-items_probability_part_one =  as.matrix(probability[, matched_part_one_columns])
+matched_part_one_trial_response = grepl("b1[0-9]+", colnames(probability))
+items_probability_part_one_full =  as.matrix(probability[, matched_part_one_trial_response])
 
-rownames(items_probability_part_one) = probability$case
+matched_part_one_answers = grepl("p1[0-9]+", colnames(probability))
+rows_without_missing_values = rowSums(is.na(probability[, matched_part_one_answers])) == 0
+items_probability_part_one_reduced = as.matrix(probability[rows_without_missing_values, matched_part_one_trial_response])
+
+rownames(items_probability_part_one_full) = probability$case
+rownames(items_probability_part_one_reduced) = probability$case[rows_without_missing_values]
 
 ## Format Q Matrix Data ----
 # Obtained from the {pks} package's data example.
@@ -33,6 +38,7 @@ rownames(qmatrix_probability_part_one) =
   sprintf("Item%02d", seq_len(nrow(qmatrix_probability_part_one)))
 
 ## Export data sets
-usethis::use_data(items_probability_part_one, overwrite = TRUE)
+usethis::use_data(items_probability_part_one_full, overwrite = TRUE)
+usethis::use_data(items_probability_part_one_reduced, overwrite = TRUE)
 usethis::use_data(qmatrix_probability_part_one, overwrite = TRUE)
 
