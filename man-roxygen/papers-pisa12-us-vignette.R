81dabcb3c305272ce01526f6b96e41cc918ac5f6
James Balamuta <balamut2@illinois.edu>
2021-11-06 23:41:06 -0500

    Add data documentation for PISA12 US Vignette

---
+++ man-roxygen/papers-pisa12-us-vignette.R
@@ -0,0 +1,16 @@
+#' @references
+#'
+#' **Assessment Design:**
+#'
+#' - PISA Test Overview: <https://www.oecd.org/pisa/aboutpisa/>
+#' - PISA 2012 Mathematics Items:
+#'  <https://www.oecd.org/pisa/test/PISA%202012%20items%20for%20release_ENGLISH.pdf>
+#'
+#' **Data used in:**
+#'
+#' - Culpepper, S. A., & Balamuta, J. J. (2021).
+#'   Inferring latent structure in polytomous data with a higher-order diagnostic model.
+#'   Multivariate Behavioral Research, 1–19. \doi{10.1080/00273171.2021.1985949}
+
+
+
